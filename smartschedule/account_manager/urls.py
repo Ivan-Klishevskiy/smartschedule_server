c18b1ede1bf5df85488eed8e14df6eca2d4277aa
Ivan Klishevskiy <i.klishevskiy@gmail.com>
2023-12-19 00:00:05 +0200

    User authorisation and registration logic.

---
+++ smartschedule/account_manager/urls.py
@@ -0,0 +1,10 @@
+# urls.py в вашем приложении
+from django.urls import re_path
+from . import views
+
+urlpatterns = [
+    re_path('login', views.login),
+    re_path('signup', views.signup),
+    re_path('test_token', views.test_token),
+
+]
