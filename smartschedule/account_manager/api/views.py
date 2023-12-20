ef8bbb0913a44a5a618615ddfa491337eb54c334
Ivan Klishevskiy <i.klishevskiy@gmail.com>
2023-12-21 00:39:13 +0200

    Authentication & Refreshing Tokens Implementation

---
+++ smartschedule/account_manager/api/views.py
@@ -0,0 +1,51 @@
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+
+from .serializers import NoteSerializer, RegisterSerializer
+from account_manager.models import Note
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        
+
+        return token
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/token',
+        '/api/token/refresh',
+    ]
+
+    return Response(routes)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getNote(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([])
+def registerUser(request):
+    serializer = RegisterSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
