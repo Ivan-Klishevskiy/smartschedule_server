e02c1644db26316699bb0b4728c24409f37a2918
Ivan Klishevskiy <i.klishevskiy@gmail.com>
2023-12-24 20:13:39 +0200

    change settings for use postgresql

--- smartschedule/smartschedule/settings.py
+++ smartschedule/smartschedule/settings.py
@@ -136,11 +136,16 @@ WSGI_APPLICATION = "smartschedule.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smartschedule_db',
+        'USER': 'ivan',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
