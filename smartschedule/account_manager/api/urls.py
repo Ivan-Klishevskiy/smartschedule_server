06a1cac9df26884ad0f3c60565bc02826b91570e
Ivan Klishevskiy <i.klishevskiy@gmail.com>
2023-12-24 22:36:04 +0200

    add test for account_manager.

--- smartschedule/account_manager/api/urls.py
+++ smartschedule/account_manager/api/urls.py
@@ -10,13 +10,13 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('', views.getRoutes),
 
-    path('user_profile/', views.getUserProfile),
+    path('user_profile/', views.getUserProfile, name='user_profile'),
     
 
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.registerUser, name='register'),
-    path('user-profile/update/', updateUserProfile, name='update-user-profile'),
+    path('user-profile/update/', updateUserProfile, name='update_user_profile'),
 
-    path('hobbies/', listHobbies, name='list-hobbies'),
+    path('hobbies/', listHobbies, name='list_hobbies'),
 ]
