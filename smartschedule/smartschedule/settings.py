63734d9a04263e2fd54a83d7aedcb21c9eee3948
Ivan Klishevskiy <i.klishevskiy@gmail.com>
2024-01-09 19:19:13 +0200

    fix cors

--- smartschedule/smartschedule/settings.py
+++ smartschedule/smartschedule/settings.py
@@ -214,6 +214,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_HEADERS = ['*']
+CORS_ALLOW_METHODS = ['*']
+
+
 
 LOGGING = {
     'version': 1,
