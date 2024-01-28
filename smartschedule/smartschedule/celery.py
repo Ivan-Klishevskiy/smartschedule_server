e33106a80d5fa86f09d15ff36c2e362ca7c6094c
Ivan Klishevskiy <i.klishevskiy@gmail.com>
2024-01-28 22:32:11 +0200

    fix start project with celery

--- smartschedule/smartschedule/celery.py
+++ smartschedule/smartschedule/celery.py
@@ -1,13 +1,14 @@
-import os
+import os,django
 import time
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartschedule.settings')
+django.setup()
 from celery import Celery
 from celery.schedules import crontab
 from django.conf import settings
 from event_manager.tasks import collect_script_contents_gout_task
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartschedule.settings')
+
 
 app = Celery('smartschedule')
 app.config_from_object('django.conf:settings')
